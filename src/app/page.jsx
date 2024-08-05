782d7bf9c23d83b20eefaff5db23eea731096014
hassaniqbal23 <hassaniqbalkiu@gmail.com>
2024-08-05 13:02:07 +0500

    create header

---
+++ src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h2>home page</h2>
+    </main>
+  );
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import { Button } from "./ui/button";
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/">
+          <h2 className="text-4xl font-semibold">
+            Hassan<span className="text-accent">.</span>
+          </h2>
+        </Link>
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button>Hire me</Button>
+          </Link>
+        </div>
+        <div className="xl:hidden">mobile nav</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
