6cd2e762b2a77661debb057825fb397cf7634199
hassaniqbal23 <hassaniqbalkiu@gmail.com>
2024-08-15 13:07:42 +0500

    create contact page

--- src/app/Contact/page.jsx
+++ src/app/Contact/page.jsx
@@ -1,10 +1,155 @@
-import React from "react";
+"use client";
+
+import { useState, FormEvent } from "react";
+import { Button } from "../../components/ui/button";
+import { Input } from "../../components/ui/input";
+import { Textarea } from "../../components/ui/textarea";
+import {
+  Select,
+  SelectContent,
+  SelectLabel,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../../components/ui/select";
+import { FaPhoneAlt, FaEnvelope, FaMapMarkedAlt } from "react-icons/fa";
+import { motion } from "framer-motion";
+
+const info = [
+  {
+    icon: <FaPhoneAlt />,
+    title: "Phone",
+    description: "(+92)355 4231410",
+  },
+
+  {
+    icon: <FaEnvelope />,
+    title: "Email",
+    description: "hassaniqbalkiu@gmail.com",
+  },
+
+  {
+    icon: <FaMapMarkedAlt />,
+    title: "Address",
+    description: "Code Corner, Teach Town 13579",
+  },
+];
 
 const Contact = () => {
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [service, setService] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission logic here
+    console.log("Form submitted");
+
+    // Clear the form fields
+    setFirstname("");
+    setLastname("");
+    setEmail("");
+    setPhone("");
+    setService("");
+    setMessage("");
+  };
+
   return (
-    <div>
-      <h2>contact page</h2>
-    </div>
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
+      className="py-6"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-[30px]">
+          {/* form */}
+          <div className="xl:h-[54%] order-2 xl:order-none">
+            <form
+              className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl"
+              onSubmit={handleSubmit}
+            >
+              <h3 className="text-4xl text-accent">Let&apos;s work together</h3>
+              <p className="text-white/60">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Explicabo deserunt ducimus consequatur pariatur aut?
+              </p>
+              {/* input */}
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <Input
+                  type="text"
+                  placeholder="Firstname"
+                  value={firstname}
+                  onChange={(e) => setFirstname(e.target.value)}
+                />
+                <Input
+                  type="text"
+                  placeholder="Lastname"
+                  value={lastname}
+                  onChange={(e) => setLastname(e.target.value)}
+                />
+                <Input
+                  type="email"
+                  placeholder="Email Address"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input
+                  type="tel"
+                  placeholder="Phone number"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                />
+              </div>
+              <Select value={service} onValueChange={setService}>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select a service" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Select a service</SelectLabel>
+                    <SelectItem value="est">Web Development</SelectItem>
+                    <SelectItem value="cst">UI/UX Design</SelectItem>
+                    <SelectItem value="mst">Logo Design</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+              <Textarea
+                className="h-[170px]"
+                placeholder="Type your message here"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+              <Button size="md" className="max-w-40">
+                Send message
+              </Button>
+            </form>
+          </div>
+          {/* Info */}
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => (
+                <li key={index}>
+                  <div>
+                    <div>{item.icon}</div>
+                  </div>
+                  <div>
+                    <p>{item.title}</p>
+                    <h3>{item.description}</h3>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </motion.section>
   );
 };
 
